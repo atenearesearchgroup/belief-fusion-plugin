6e123a304f5d4c8dca46d2a3a4c23d22df977d4a
lburgueno <lolaburgueno@gmail.com>
2021-11-09 17:36:51 +0100

    SBoolean literals with base rate

--- plugin/BeliefFusionPlugin/src/fusionPlugin/Belief.java
+++ plugin/BeliefFusionPlugin/src/fusionPlugin/Belief.java
@@ -16,7 +16,11 @@ public class Belief {
 	private final String SBOOLEAN_REGEX = "SBoolean(\\s)*\\(((([0-9]+)(\\.[0-9]+)?)(\\s)*,(\\s)*){3}((([0-9]+)(\\.[0-9]+)?)){1}(\\s)*\\)";
 	private final String UBOOLEAN_REGEX = "UBoolean(\\s)*\\((\\s)*(true|false)(\\s)*,(\\s)*((([0-9]+)(\\.[0-9]+)?)){1}(\\s)*\\)";
 	private final String BOOLEAN_REGEX = "(true|false)";
-	private final String DEGREE_OF_CERTAINTY_CERTAIN = "CERTAIN"; 
+	
+//	private enum ULiterals {CERTAIN, PROBABLE, POSSIBLE, UNCERTAIN, IMPROBABLE, UNLIKELY, IMPOSSIBLE };
+	private final String SBOOLEAN_LITERAL_REGEX = "SBoolean(\\s)*\\([a-zA-Z]*((\\s)*,(\\s)*){1}((([0-9]+)(\\.[0-9]+)?)){1}(\\s)*\\)";
+			
+	private final String DEGREE_OF_CERTAINTY_CERTAIN = "CERTAIN"; 		 
 	private final String DEGREE_OF_CERTAINTY_PROBABLE = "PROBABLE";
 	private final String DEGREE_OF_CERTAINTY_POSSIBLE = "POSSIBLE";		 
 	private final String DEGREE_OF_CERTAINTY_UNCERTAIN = "UNCERTAIN";
@@ -29,37 +33,30 @@ public class Belief {
 	
 	public Belief(String agent, LiteralString opinion) {
 		this.agent = agent;
-		this.opinion = parseValue(opinion.getValue().toString().toUpperCase());
+		this.opinion = parseValue(opinion.getValue().toString());
 	}
 	
 	private SBoolean parseValue(String opinion) throws InputMismatchException {
+			
 		Pattern sbooleanPattern = Pattern.compile(SBOOLEAN_REGEX);
 		Matcher sbooleanMatcher = sbooleanPattern.matcher(opinion);
 		
 		Pattern ubooleanPattern = Pattern.compile(UBOOLEAN_REGEX);
 		Matcher ubooleanMatcher = ubooleanPattern.matcher(opinion);
 		
+		Pattern sbooleanliteralPattern = Pattern.compile(SBOOLEAN_LITERAL_REGEX);
+		Matcher sbooleanliteralMatcher = sbooleanliteralPattern.matcher(opinion);
+		
 		Pattern booleanPattern = Pattern.compile(BOOLEAN_REGEX);
 		Matcher booleanMatcher = booleanPattern.matcher(opinion);
 		
 		SBoolean result;
-		System.out.println("Opinion" + opinion);
-		System.out.println(opinion.equals(DEGREE_OF_CERTAINTY_PROBABLE));
+//		System.out.println("Opinion" + opinion);
+//		System.out.println(opinion.equals(DEGREE_OF_CERTAINTY_PROBABLE));
 		
-		if (opinion.toUpperCase().equals(DEGREE_OF_CERTAINTY_CERTAIN)) {
-			result =  new SBoolean(1, 0, 0, 0.5);
-		} else if (opinion.toUpperCase().equals(DEGREE_OF_CERTAINTY_PROBABLE)) {
-			result =  new SBoolean(0.67, 0, 0.33, 0.5);
-		} else if (opinion.toUpperCase().equals(DEGREE_OF_CERTAINTY_POSSIBLE)){
-			result =  new SBoolean(0.33, 0, 0.67, 0.5);
-		} else if (opinion.toUpperCase().equals(DEGREE_OF_CERTAINTY_UNCERTAIN)){
-			result =  new SBoolean(0, 0, 1, 0.5);
-		} else if (opinion.toUpperCase().equals(DEGREE_OF_CERTAINTY_IMPROBABLE)){
-			result =  new SBoolean(0, 0.33, 0.67, 0.5);
-		} else if (opinion.toUpperCase().equals(DEGREE_OF_CERTAINTY_UNLIKELY)){
-			result =  new SBoolean(0, 0.67, 0.33, 0.5);
-		} else if (opinion.toUpperCase().equals(DEGREE_OF_CERTAINTY_IMPOSSIBLE)){
-			result =  new SBoolean(0, 1, 0, 0.5);
+	
+		if (sbooleanliteralMatcher.matches()) {
+			result = parseSBooleanLiteral(opinion);
 		} else if (sbooleanMatcher.matches()) {
 			result =  parseSBoolean(opinion);
 		} else if (ubooleanMatcher.matches()) {
@@ -84,6 +81,37 @@ public class Belief {
 			}
 		}
 		return new SBoolean(values[0], values[1], values[2], values[3]);
+	}
+	
+	private SBoolean parseSBooleanLiteral(String opinion) {
+		String[] tokens = opinion.split("SBoolean\\(|,|\\)");
+
+
+		System.out.println("TOKENS" + tokens);
+		String literal = tokens[1];
+		double baseRate = Double.parseDouble(tokens[2]);
+				
+		SBoolean result; 
+		
+		if (literal.equals(DEGREE_OF_CERTAINTY_CERTAIN)) {
+			result =  new SBoolean(1, 0, 0, baseRate);
+		} else if (literal.equals(DEGREE_OF_CERTAINTY_PROBABLE)) {
+			result =  new SBoolean(0.67, 0, 0.33, baseRate);
+		} else if (literal.equals(DEGREE_OF_CERTAINTY_POSSIBLE)){
+			result =  new SBoolean(0.33, 0, 0.67, baseRate);
+		} else if (literal.equals(DEGREE_OF_CERTAINTY_UNCERTAIN)){
+			result =  new SBoolean(0, 0, 1, baseRate);
+		} else if (literal.equals(DEGREE_OF_CERTAINTY_IMPROBABLE)){
+			result =  new SBoolean(0, 0.33, 0.67, baseRate);
+		} else if (literal.equals(DEGREE_OF_CERTAINTY_UNLIKELY)){
+			result =  new SBoolean(0, 0.67, 0.33, baseRate);
+		} else if (literal.equals(DEGREE_OF_CERTAINTY_IMPOSSIBLE)){
+			result =  new SBoolean(0, 1, 0, baseRate);
+		} else {
+			throw new InputMismatchException("Input string " + opinion + " does not match a valid Belief value.");
+		}
+		
+		return result;
 	}
 	
 	private SBoolean parseUBoolean(String opinion) {
